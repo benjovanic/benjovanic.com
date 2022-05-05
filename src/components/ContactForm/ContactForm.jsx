e2577892396ddd4945f3237144b2b7999b947726
Ben Jovanic <benjovanic6@gmail.com>
2022-05-04 19:54:36 -0700

    Hide form after submit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -10,6 +10,7 @@ const ContactForm = () => {
   const [data, setData] = useState({});
   const [messages, setMessages] = useState(null);
   const [sending, setSending] = useState(false);
+  const [sent, setSent] = useState(false);
 
   useEffect(() => {
     // the API is running on a free Heroku pod so this initial call helps wake it up
@@ -70,10 +71,11 @@ const ContactForm = () => {
       api.contact(data)
         .then(() => {
           setMessages({
-            success: 'Your message has been sent, I\'ll be in touch soon.',
+            success: 'Thanks for message, I\'ll be in touch soon.',
           });
           setData({});
           setSending(false);
+          setSent(true);
           document.getElementById('form').reset();
         })
         .catch(() => {
@@ -84,6 +86,8 @@ const ContactForm = () => {
         });
     }
   };
+
+  const resetForm = () => setSent(false);
 
   return (
     <form
@@ -101,70 +105,85 @@ const ContactForm = () => {
           {messages?.error || messages?.success}
         </Typography>
       )}
-      <Grid item xs={12} style={{ marginTop: 10 }}>
-        Name
-        <TextField
-          id="form-name"
-          name="name"
-          variant="outlined"
-          onChange={handleChange}
-          onBlur={() => formValid('name')}
-          required
-          error={!!messages?.name}
-        />
-        {messages?.name && (
-          <Typography align="left" component="p" gutterBottom>
-            {messages?.name}
-          </Typography>
-        )}
-      </Grid>
-      <Grid item xs={12} style={{ marginTop: 10 }}>
-        Email
-        <TextField
-          type="email"
-          id="form-email"
-          name="email"
-          variant="outlined"
-          onChange={handleChange}
-          onBlur={() => formValid('email')}
-          required
-          error={!!messages?.email}
-        />
-        {messages?.email && (
-          <Typography align="left" component="p" gutterBottom>
-            {messages?.email}
-          </Typography>
-        )}
-      </Grid>
-      <Grid item xs={12} style={{ marginTop: 10 }}>
-        Message
-        <TextField
-          id="form-message"
-          name="message"
-          variant="outlined"
-          onChange={handleChange}
-          onBlur={() => formValid('message')}
-          required
-          multiline
-          rows={4}
-          error={!!messages?.message}
-        />
-        {messages?.message && (
-          <Typography align="left" component="p" gutterBottom>
-            {messages?.message}
-          </Typography>
-        )}
-      </Grid>
-      <Grid item xs={12} style={{ marginTop: 10 }}>
-        <Button
-          style={{ backgroundColor: '#fff' }}
-          variant="contained"
-          type="submit"
-          disabled={sending}
-        >
-          {sending ? 'Sending...' : 'Send'}
-        </Button>
-      </Grid>
+      {!sent ? (
+        <>
+          <Grid item xs={12} style={{ marginTop: 10 }}>
+            Name
+            <TextField
+              id="form-name"
+              name="name"
+              variant="outlined"
+              onChange={handleChange}
+              onBlur={() => formValid('name')}
+              required
+              error={!!messages?.name}
+            />
+            {messages?.name && (
+              <Typography align="left" component="p" gutterBottom>
+                {messages?.name}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item xs={12} style={{ marginTop: 10 }}>
+            Email
+            <TextField
+              type="email"
+              id="form-email"
+              name="email"
+              variant="outlined"
+              onChange={handleChange}
+              onBlur={() => formValid('email')}
+              required
+              error={!!messages?.email}
+            />
+            {messages?.email && (
+              <Typography align="left" component="p" gutterBottom>
+                {messages?.email}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item xs={12} style={{ marginTop: 10 }}>
+            Message
+            <TextField
+              id="form-message"
+              name="message"
+              variant="outlined"
+              onChange={handleChange}
+              onBlur={() => formValid('message')}
+              required
+              multiline
+              rows={4}
+              error={!!messages?.message}
+            />
+            {messages?.message && (
+              <Typography align="left" component="p" gutterBottom>
+                {messages?.message}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item xs={12} style={{ marginTop: 10 }}>
+            <Button
+              style={{ backgroundColor: '#fff' }}
+              variant="contained"
+              type="submit"
+              disabled={sending}
+            >
+              {sending ? 'Sending...' : 'Send'}
+            </Button>
+          </Grid>
+        </>
+      ) : (
+        <p>
+          <Button
+            style={{ backgroundColor: '#fff' }}
+            variant="contained"
+            type="button"
+            onClick={resetForm}
+          >
+            Send another message
+          </Button>
+        </p>
+      )}
     </form>
   );
 };
