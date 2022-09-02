645164cd6554826e82e5ffb9ee95ddc1945b8a6b
Ben Jovanic <benjovanic6@gmail.com>
2022-09-02 07:22:58 -0700

    Adjust photo

--- src/components/PersonalSummary/PersonalSummary.jsx
+++ src/components/PersonalSummary/PersonalSummary.jsx
@@ -9,7 +9,7 @@ const PersonalSummary = () => (
       <a href="/">
         <Avatar
           alt="Ben Jovanic"
-          src="ben-jovanic.jpg"
+          src="ben-jovanic.jpg?v=2"
           style={{ height: 180, width: 180, margin: 'auto' }}
         />
       </a>
